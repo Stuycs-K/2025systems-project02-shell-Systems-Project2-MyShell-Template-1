d0d669c7619bacf856222d338d13cad2c2c4f983
Renxuan Pang <rpang50@stuy.edu>
2024-12-05 14:30:14 -0500

    typo

--- functions.h
+++ functions.h
@@ -3,6 +3,6 @@
 void err();
 void printCWD();
 int parse_args( char * line, char ** arg_ary );
-int redirection(int dest, int source);
+int redirection(int source, int dest);
 int run(char** argAry);
 #endif
